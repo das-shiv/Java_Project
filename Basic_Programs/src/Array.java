c37b3bb970d2adbb42372c80acb57eecc7fb63d5
shiv <ssdas.in@gmail.com>
2024-12-24 23:20:23 +0530

    Added Notes directory, and addded some comments to the previous codes to explain the code better

--- Basic_Programs/src/Array.java
+++ Basic_Programs/src/Array.java
@@ -4,20 +4,21 @@ public class Array {
     public static void main(String[] args) {
         System.out.println("This program takes array of numbers as input and produces a sum operation.");
 
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);  //created a scanner to take user input
         System.out.print("How many numbers you want to add ? ");
-        int count = input.nextInt();
-        Double num[] = new Double[count];
+        int count = input.nextInt();  //count variable stores the number of items user wants to give, i.e. the total number of items user wants to add.
+        Double num[] = new Double[count]; // created am array named num that stores double type data.
 
         System.out.println("Please enter " + count + " number of elements.");
+        // this loop runs for the number of times user wants to enter data
         for (int i = 0; i < count; i++) {
             System.out.print("Please enter the next number : ");
-            num[i] = input.nextDouble();
+            num[i] = input.nextDouble();  // storing the user inputs in num array
 
         }
 
-        SumOfArray obj = new SumOfArray();
-        double result = obj.sum(num);
+        SumOfArray obj = new SumOfArray();  // creating an object of the SumOfArray class
+        double result = obj.sum(num);   //calling the sum method from SumOfArray class, passing the num array as parameter and storing the return in result variable
 
         System.out.println("The total sum of your numbers is :" + result );
 
--- Basic_Programs/src/SumOfArray.java
+++ Basic_Programs/src/SumOfArray.java
@@ -1,11 +1,12 @@
 class SumOfArray {
+    // the sum method takes an array of double type as parameter and stores in numbers array
     public double sum(Double[] numbers){
-        int len = numbers.length;
-        double total = 0;
+        int len = numbers.length; // finding the length of the array numbers, so that many times we have to run the loop to add the items
+        double total = 0; // total sum of numbers initialized to 0
         for(int i=0; i< len; i++){
-            total = total + numbers[i];
+            total = total + numbers[i]; // for each iteration the ith item of the array is getting added to total
         }
 
-        return total;
+        return total; // returns the total after the method is done executing
     }
 }
