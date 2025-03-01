c5a8de148b5464716ecf0d3a62cbfdf3a384aac7
shiv <ssdas.in@gmail.com>
2025-03-01 19:26:35 +0530

    Update

---
+++ OOP_Codes/src/StringBufferr.java
@@ -0,0 +1,9 @@
+public class StringBufferr {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("shiv");
+        System.out.println(sb);
+        sb.append("das");
+        System.out.println(sb);
+    }
+    
+}
